50bb36a4e197df3c4815afda7c3fe8eca7b68146
hjpark9999 <hjpark9999@knou.ac.kr>
2021-04-13 22:15:19 +0900

    loop2 특별문제2 루프 수정한것

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -4,19 +4,10 @@ void main()
 	int j = 2;
 	int i = 1;
 	int count=0;
+	int outloop = 0;
 
-		while (i <= 4) {
-			count = 1;		
-			while (count <= i) {
-				printf("%d   ", j);
-				j += 2;
-				count++;
-			}
-			printf("\n");
-			i++;
-		}
-		i = 1;
-		j = 22;
+	while (outloop < 2) {
+
 		while (i <= 4) {
 			count = 1;
 			while (count <= i) {
@@ -27,5 +18,9 @@ void main()
 			printf("\n");
 			i++;
 		}
+		i = 1;
+		j = 22;
+		outloop++;
+	}
 	fgetc(stdin);
 }
